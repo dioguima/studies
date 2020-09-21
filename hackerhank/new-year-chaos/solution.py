db7c6f789d521b95b51e61196b669447c8a0b51d
Diogo Guimarães <diogoferreiraguima@hotmail.com>
2020-09-21 23:22:56 +0200

    New year  chaos problem resolved

--- hackerhank/new-year-chaos/solution.py
+++ hackerhank/new-year-chaos/solution.py
@@ -8,7 +8,15 @@ import sys
 
 # Complete the minimumBribes function below.
 def minimumBribes(q):
-    print(1)
+    answer = 0
+    for i in range(len(q)):
+        if q[i] - (i + 1) > 2:
+            print('Too chaotic')
+            return
+        for j in range(max(0, q[i] - 2), i):
+            if(q[j] > q[i]):
+                answer += 1
+    print(answer)
 
 if __name__ == '__main__':
     t = int(input())
