93ab430a250b94380a95bec77414b731758e95a8
Diogo Guimarães <diogoferreiraguima@hotmail.com>
2020-09-29 22:58:17 +0200

    Minor fix on assert.py

--- HackerRank/assert.py
+++ HackerRank/assert.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     in_file_paths = glob.glob(f'{folder_path}/*.in')
     expected_file_paths = glob.glob(f'{folder_path}/*.expected')
     
-    i = 0
-    while i < len(in_file_paths):
+    
+    for i in range(len(in_file_paths)):
         execution_subprocess_result = subprocess.run(f'powershell.exe cat {in_file_paths[i]} | python {folder_path}/solution.py', shell=True, capture_output=True)
         expected_read_subprocess_result = subprocess.run(f'powershell.exe cat {expected_file_paths[i]}', shell=True, capture_output=True)
 
@@ -21,4 +21,3 @@ if __name__ == "__main__":
         else:
             print(f'ERROR\n\nResult of file {in_file_paths[i]} was:')
             print(f'\n\n{result_execution}\n\nExpected was:\n\n{expected_result}\n\n')
-        i += 1
--- README.md
+++ README.md
@@ -9,5 +9,5 @@ This the folder that solutions of [HakerRank](https://www.hackerrank.com/) probl
 The script [assert.py](HackerRank/assert.py) is a script that is used to input data, run the solutions and compare with the expected result for every problem solved. The usage is simple, the parameter is the folder that contains the solution, the inputs and expected result files (the files must follow the patter N.in for inputs or N.expected for expected results, N is the index of test case)
 
 ```bash
-python assert.py new-york-chaos
+python .\assert.py .\repeated-string\
 ```
