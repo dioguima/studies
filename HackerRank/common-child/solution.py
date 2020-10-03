86016c5c9435a6e5de0770be24185bdb58e3fafd
Diogo Guimarães <diogoferreiraguima@hotmail.com>
2020-10-03 22:16:47 +0200

    Common child problem solution finished, still not performatic enought

--- HackerRank/common-child/solution.py
+++ HackerRank/common-child/solution.py
@@ -5,37 +5,31 @@ import os
 import random
 import re
 import sys
+import collections
 
 # Complete the commonChild function below.
+# def commonChild(s1, s2):
+#     result = None
+#     if len(s1) == 0 or len(s2) == 0:
+#         result = 0
+#     elif s1[-1] == s2[-1]:
+#         result = 1 + commonChild(s1[:-1], s2[:-1])
+#     else:
+#         result = max(commonChild(s1[:-1], s2), commonChild(s1, s2[:-1]))
+#     return result
+
 def commonChild(s1, s2):
-    chars_count_s1 = {}
-    chars_count_s2 = {}
-
-    for i in range(len(s1)):
-        char_s1 = s1[i]
-        if chars_count_s1.get(char_s1) == None:
-            chars_count_s1[char_s1] = 1
-        else:
-            chars_count_s1[char_s1] += 1
-        
-        char_s2 = s2[i]
-        if chars_count_s2.get(char_s2) == None:
-            chars_count_s2[char_s2] = 1
-        else:
-            chars_count_s2[char_s2] += 1
-
-    # print(chars_count_s1)
-    # print(chars_count_s2)
-
-    for i in range(len(s1)):
-        current_char_s1 = s1[i]
-        current_best_s1 = current_char_s1
-        # for j in range(len(s2)):
-        #     if chars_count_s2.get(current_char_s1) != None:
-                
-
-
-    return -1
+    len_s1 = len(s1)
+    len_s2 = len(s2)
+    matrix = [[0 for j in range(len_s2 + 1)] for i in range(len_s1 + 1)]
+    
+    for i1 in range(len_s1):
+        for i2 in range(len_s2):
+            if s1[i1] == s2[i2]:
+                matrix[i1 + 1][i2 + 1] = 1 + matrix[i1][i2]
+            else:
+                matrix[i1 + 1][i2 + 1] = max( matrix[i1][i2 + 1], matrix[i1 + 1][i2])
+    return matrix[len_s1][len_s2]
 
 if __name__ == '__main__':
 
