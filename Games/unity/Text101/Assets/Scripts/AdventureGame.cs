48bdc4268d1294b48c14b29b5ade783e791296c6
Diogo Guimarães <diogoferreiraguima@hotmail.com>
2020-11-01 21:39:10 +0100

    Finished the devlopmnet of Text101

--- Games/unity/Text101/Assets/Scripts/AdventureGame.cs
+++ Games/unity/Text101/Assets/Scripts/AdventureGame.cs
@@ -34,12 +34,13 @@ public class AdventureGame : MonoBehaviour
     private void ManageState()
     {
         var nextStates = state.GetNextStates();
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+
+        for (int index = 0; index < nextStates.Length; index++)
         {
-            state = nextStates[0];
-        }else if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            state = nextStates[1];
+            if (Input.GetKeyDown(KeyCode.Alpha1 + index))
+            {
+                state = nextStates[index];
+            }
         }
 
         textComponent.text = state.GetStateStory();
