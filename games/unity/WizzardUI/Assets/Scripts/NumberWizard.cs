5dbb6b4931e7ae8fbace0e4fddbdc1145adf7c9e
Diogo Guimarães <diogoferreiraguima@hotmail.com>
2021-02-10 23:13:23 +0100

    Created number wizard script

---
+++ games/unity/WizzardUI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+    [SerializeField]
+    int max = 1000;
+    [SerializeField]
+    int min = 1;
+    int guess = 500;
+
+    // Use this for initialization
+    void Start ()
+    {
+        max += 1;
+        NextGuess();
+	}
+	
+    public void OnPressHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+    }
+}
